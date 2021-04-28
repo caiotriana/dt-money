43e9ccd6996b06e195c9b62de8eef3198fa10470
Caio Triana <caio_triana@hotmail.com>
2021-04-28 19:25:22 -0300

    adding react modal on project and separate files to use

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,20 @@
 import logo from '../../assets/logo.svg'
 import { HeaderContainer, HeaderContent } from './styles'
 
-export function Header(){
+interface HeaderProps { 
+  onOpenNewTransactionModal : () => void;
+}
+
+export function Header({onOpenNewTransactionModal}: HeaderProps){
+
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logo} alt="dt money" />
-        <button type="button">
+        <button type="button" onClick={onOpenNewTransactionModal}>
           Nova transação
         </button>
+
       </HeaderContent>
     </HeaderContainer>
   )
